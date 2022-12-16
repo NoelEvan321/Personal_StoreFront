49a42f5cb0f669e277ad02f695313217a4329d13
NoelEvan321 <noel_evan@outlook.com>
2022-12-16 09:51:54 -0600

    Fixed the create post code for the image file uplaods.

--- Personal_StoreFront/Personal_StoreFront.UI.MVC/Controllers/ProductsController.cs
+++ Personal_StoreFront/Personal_StoreFront.UI.MVC/Controllers/ProductsController.cs
@@ -10,6 +10,7 @@ using Personal_StoreFront.DATA.EF.Models;
 using Microsoft.AspNetCore.Authorization;
 using System.Drawing;
 using Personal_StoreFront.UI.MVC.Controllers;
+using Personal_StoreFront.UI.MVC.Utilities;
 
 namespace Personal_StoreFront.UI.MVC.Controllers
 {
@@ -86,6 +87,7 @@ namespace Personal_StoreFront.UI.MVC.Controllers
         }
 
         // GET: Products/Create
+        [Authorize(Roles = "Admin")]
         public IActionResult Create()
         {
             ViewData["CardConditionId"] = new SelectList(_context.CardConditions, "CardConditionId", "Condition");
@@ -103,6 +105,63 @@ namespace Personal_StoreFront.UI.MVC.Controllers
         {
             if (ModelState.IsValid)
             {
+
+                #region File Upload - CREATE
+                //Check to see if a file was uploaded
+                if (product.Image != null)
+                {
+                    //Check the file type 
+                    //- retrieve the extension of the uploaded file
+                    string ext = Path.GetExtension(product.Image.FileName);
+
+                    //- Create a list of valid extensions to check against
+                    string[] validExts = { ".jpeg", ".jpg", ".gif", ".png" };
+
+                    //- verify the uploaded file has an extension matching one of the extensions in the list above
+                    //- AND verify file size will work with our .NET app
+                    if (validExts.Contains(ext.ToLower()) && product.Image.Length < 4_194_303)//underscores don't change the number, they just make it easier to read
+                    {
+                        //Generate a unique filename
+                        product.ProductPicture = Guid.NewGuid() + ext;
+
+                        //Save the file to the web server (here, saving to wwwroot/images)
+                        //To access wwwroot, add a property to the controller for the _webHostEnvironment (see the top of this class for our example)
+                        //Retrieve the path to wwwroot
+                        string webRootPath = _webHostEnvironment.WebRootPath;
+                        //variable for the full image path --> this is where we will save the image
+                        string fullImagePath = webRootPath + "/img/";
+
+                        //Create a MemoryStream to read the image into the server memory
+                        using (var memoryStream = new MemoryStream())
+                        {
+                            await product.Image.CopyToAsync(memoryStream);//transfer file from the request to server memory
+                            using (var img = Image.FromStream(memoryStream))//add a using statement for the Image class (using System.Drawing)
+                            {
+                                //now, send the image to the ImageUtility for resizing and thumbnail creation
+                                //items needed for the ImageUtility.ResizeImage()
+                                //1) (int) maximum image size
+                                //2) (int) maximum thumbnail image size
+                                //3) (string) full path where the file will be saved
+                                //4) (Image) an image
+                                //5) (string) filename
+                                int maxImageSize = 500;//in pixels
+                                int maxThumbSize = 100;
+
+                                ImageUtility.ResizeImage(fullImagePath, product.ProductPicture, img, maxImageSize, maxThumbSize);
+                                //myFile.Save("path/to/folder", "filename"); - how to save something that's NOT an image
+
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    //If no image was uploaded, assign a default filename
+                    //Will also need to download a default image and name it 'noimage.png' -> copy it to the /images folder
+                    product.ProductPicture = "noimage.png";
+                }
+
+                #endregion
 
                 _context.Add(product);
                 await _context.SaveChangesAsync();
